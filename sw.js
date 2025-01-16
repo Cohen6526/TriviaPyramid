648a4985e0e89c59ae9f7a3eec4dcb9687f87c5d
Cohen6526 <cohenteschke@gmail.com>
2025-01-16 10:18:05 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 'v152';  // change this everytime you update the service worker
+const version = 'v153';  // change this everytime you update the service worker
                           // to force the browser to also update it.
 
 
